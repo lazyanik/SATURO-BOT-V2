3cf89cff02c4865096c4b15e6f4c9d5cb972426b
🦋 ꫝɴ֟፝ɪᴋ ɪsʟꫝᴍ ѕꫝᴅɪᴋ ♡ <mdsadikbhaiproo@gmail.com>
2026-08-20 16:23:50 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -2,19 +2,21 @@ module.exports = {
   config: {
     name: "fork",
     aliases: ["repo", "source"],
-    version: "1.0",
-    author: "NeoKEX",
+    version: "2.0",
+    author: "Anik Islam Sadik",
     countDown: 3,
     role: 0,
-    longDescription: "Returns the link to the official, updated fork of the bot's repository.",
+    longDescription: "Returns the link to the official repository.",
     category: "system",
     guide: { en: "{pn}" }
   },
 
   onStart: async function({ message }) {
-    const text = "✓ | Here is the updated repository:\n\nhttps://github.com/lazyneoaz/Goatbot-V2.git\n\n" +
-                 "Changes:\n1. No Google Credentials needed\n2. Enhanced overall performance\n3. Now using @lazyneoaz/nkxfca [ Automatic log out issues Solved ]\n4. Working on all groups\n5. Id Ban Issue solved 90% and running for a long time\n\nNB: If you want to use @lazyneoaz/nkxfca please install by typing: npm i @lazyneoaz/nkxfca@latest\n\n" +
-                 "Keep supporting^_^";
+    const text = "╭•┈•『 🤍 𝐒𝐀𝐓𝐔𝐑𝐎-𝐁𝐎𝐓-𝐕𝟐 💖 』•┈•╮\n\n" +
+                 "⚡ 𝐒𝐭𝐚𝐭𝐮𝐬 ──► 𝐀𝐜𝐭𝐢𝐯𝐞 & 𝐒𝐭𝐚𝐛𝐥𝐞\n" +
+                 "👑 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ──► 𝐀𝐧𝐢𝐤 𝐈𝐬𝐥𝐚𝐦 𝐒𝐚𝐝𝐢𝐤\n" +
+                 "🔗 𝐑𝐞𝐩𝐨 ────► https://github.com/lazyanik/SATURO-BOT-V2.git\n\n" +
+                 "╰•┈•『 ✓ 𝐊𝐞𝐞𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 ❤️ 』•┈•╯";
     
     message.reply(text);
   }
