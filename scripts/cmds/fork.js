20ff9c01c90d1e04841e460a214b2f6073fcd01e
🦋 ꫝɴ֟፝ɪᴋ ɪsʟꫝᴍ ѕꫝᴅɪᴋ ♡ <mdsadikbhaiproo@gmail.com>
2026-09-06 15:01:09 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -12,12 +12,14 @@ module.exports = {
   },
 
   onStart: async function({ message }) {
-    const text = "╭•┈•『 🤍 𝐒𝐀𝐓𝐔𝐑𝐎-𝐁𝐎𝐓-𝐕𝟐 💖 』•┈•╮\n\n" +
-                 "⚡ 𝐒𝐭𝐚𝐭𝐮𝐬 ──► 𝐀𝐜𝐭𝐢𝐯𝐞 & 𝐒𝐭𝐚𝐛𝐥𝐞\n" +
-                 "👑 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ──► 𝐀𝐧𝐢𝐤 𝐈𝐬𝐥𝐚𝐦 𝐒𝐚𝐝𝐢𝐤\n" +
-                 "🔗 𝐑𝐞𝐩𝐨 ────► https://github.com/lazyanik/SATURO-BOT-V2.git\n\n" +
-                 "╰•┈•『 ✓ 𝐊𝐞𝐞𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 ❤️ 』•┈•╯";
+    const text = `╭━━━ 〈 𝐒𝐀𝐓𝐔𝐑𝐎-𝐁𝐎𝐓-𝐕𝟐 〉 ━━━╮
+        
+✧ 𝐒𝐭𝐚𝐭𝐮𝐬  ➪ 🟢 𝐀𝐜𝐭𝐢𝐯𝐞 & 𝐒𝐭𝐚𝐛𝐥𝐞
+✧ 𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ➪ 👑 𝐀𝐧𝐢𝐤 𝐈𝐬𝐥𝐚𝐦 𝐒𝐚𝐝𝐢𝐤
+✧ 𝐆𝐢𝐭𝐇𝐮𝐛  ➪ 🔗 https://github.com/lazyanik/SATURO-BOT-V2.git
+
+╰━━━ 〈 𝐊𝐞𝐞𝐩 𝐒𝐮𝐩𝐩𝐨𝐫𝐭𝐢𝐧𝐠 ❤️ 〉 ━━━╯`;
     
-    message.reply(text);
+    await message.reply(text);
   }
 };
