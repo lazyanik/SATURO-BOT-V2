f11cf4597d6af1cecdba78b0cfebda319f4e14a8
🦋 ꫝɴ֟፝ɪᴋ ɪsʟꫝᴍ ѕꫝᴅɪᴋ ♡ <mdsadikbhaiproo@gmail.com>
2026-09-21 00:31:34 +0600

    Update pair.js

--- scripts/cmds/pair.js
+++ scripts/cmds/pair.js
@@ -29,7 +29,7 @@ async function toFont(text, id = 21) {
 module.exports = {
  config: {
  name: "pair",
- aliases: ["pr", "lovepair", "match"],
+ aliases: ["pr"],
  author: "Saimx69x",
  version: "2.0",
  role: 0,
